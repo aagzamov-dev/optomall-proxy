b59fd8551b25b8515a25f5ae91da0ec462da9260
abdurashid <kingdev5809@gmail.com>
2026-03-22 14:14:37 +0500

    Some changes

--- index.js
+++ index.js
@@ -57,4 +57,4 @@ app.get("/image-proxy", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Proxy running on port 3000"));
+app.listen(3003, () => console.log("Proxy running on port 3003"));
